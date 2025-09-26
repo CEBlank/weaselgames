dea0ba9364daafdd064bd596c5728d5fa12673bd
CEBlank <96022511+CEBlank@users.noreply.github.com>
2025-09-26 01:27:49 -0500

    moar testing

--- weasel-games-app/src/App.tsx
+++ weasel-games-app/src/App.tsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import './index.css'
 import './App.css'
+
+
 
 function App() {
 
   return (
     <>
-      <div className="navbar bg-base-100 shadow-sm">
 
-        <div className='w-40 h-40'>
-          <img id="LogoWeasel" src="/images/ermineLogo.png" alt="Weasel Logo"/>
-        </div>
+      <ul className="navbar menu lg:menu-horizontal bg-base-100 rounded-box ">
+        <li>
+          <a>
+            <img className='w-40 h-40' src='images/ermineLogo.png'/>
+          </a>
+        </li>
+        <li>
+          <h1 className='text-2xl bold'>Weasel Games</h1>
+        </li>  
 
-        <div className="flex-none">
-          <button className="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path> </svg>
-          </button>
-        </div>
+        <li>
+          <details>
+            <summary>Parent item</summary>
+            <ul>
+              <li><a>Submenu 1</a></li>
+              <li><a>Submenu 2</a></li>
+              <li>
+                <details>
+                  <summary>Parent</summary>
+                  <ul>
+                    <li><a>item 1</a></li>
+                    <li><a>item 2</a></li>
+                  </ul>
+                </details>
+              </li>
+            </ul>
+          </details>
+        </li>
+        <li><a>Item 3</a></li>
+      </ul>
 
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">Weasel Games</a>
-        </div>
-        
-{/*         <div className="flex-none">
-          <button className="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path> </svg>
-          </button>
-        </div> */}
-      </div>
     </>
   )
 }
