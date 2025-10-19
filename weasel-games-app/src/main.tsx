301bfd8b190f46f07ef336f90486ae6f94cd8a82
Chris <96022511+CEBlank@users.noreply.github.com>
2025-10-19 00:14:24 -0500

    Update main.tsx

--- weasel-games-app/src/main.tsx
+++ weasel-games-app/src/main.tsx
@@ -18,7 +18,7 @@ createRoot(document.getElementById('root')!).render(
           domain="dev-ug4olss0uf5d2qw0.us.auth0.com"
           clientId="dR8CFGtMryQoFFVYIZ6bZoGk1tqzZwJ0"
           authorizationParams={{
-            redirect_uri: 'window.location.origin'
+            redirect_uri: 'https://weaselgames.vercel.app/'
           }}
         >
 
