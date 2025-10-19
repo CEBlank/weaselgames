a15e7b3261ddb06b3d3cf31add15293d8c4b6ffe
Chris <96022511+CEBlank@users.noreply.github.com>
2025-10-19 18:41:50 -0500

    events page build
    events, initial calendar load in
    rough outline page plan

--- weasel-games-app/src/events.tsx
+++ weasel-games-app/src/events.tsx
@@ -2,7 +2,40 @@ export function Events() {
 
   return (
     <>
-    Events Page
+    <div
+      id='eventsHead'
+      className="text-center">
+      <h1>Events</h1>
+    </div>
+      
+    <div className="grid grid-cols-9 gap-4">
+      <div className="col-start-2" >
+        <iframe 
+        
+        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FChicago&showPrint=0&src=d2Vhc2VsZ2FtZWNvQGdtYWlsLmNvbQ&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039be5&color=%230b8043" 
+        width="1000" 
+        height="1000"
+        ></iframe>
+      </div>
+
+      <div
+      className="col-start-7 col-span-1">
+
+
+      </div>
+    </div>
+
+    <div
+      id='eventsHead'
+      className="mt-2 text-center">
+      <h1>Past Events</h1>
+
+
+
+
+    </div>
+
+
     </>
   )
 }
