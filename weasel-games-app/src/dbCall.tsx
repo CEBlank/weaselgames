19c0a9b1bde81c046c1ca8d71de88fd62a963508
Chris <96022511+CEBlank@users.noreply.github.com>
2025-11-02 19:24:31 -0600

    node install pain
    give a moose a muffin, and he'll ask if you have this other CLI installed

---
+++ weasel-games-app/src/dbCall.tsx
@@ -0,0 +1,7 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// Replace the placeholders in the connection string uri with your credentials
+const uri = "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority";
+
+// Create a client with options to specify Stable API Version 1
+const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
