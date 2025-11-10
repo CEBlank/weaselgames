d76c5d2e2b3dabecfd0387b0aa32656725fe40ec
Chris <96022511+CEBlank@users.noreply.github.com>
2025-11-09 21:44:55 -0600

    shop page tweaks

--- weasel-games-app/src/shop.tsx
+++ weasel-games-app/src/shop.tsx
@@ -67,8 +67,6 @@ const products = [
   },
 ]
 
-
-
   return (
     <div className="">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 bg-white">
@@ -76,15 +74,17 @@ const products = [
 
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 bg-white">
           {products.map((product) => (
-            <a key={product.id} href={product.href} className="group">
-              <img
-                alt={product.imageAlt}
-                src={product.imageSrc}
+            <span key={product.id} 
+                  // href={product.href} 
+                  className="group">
+              <p
+                /* alt={product.imageAlt}
+                src={product.imageSrc} */
                 className="aspect-square w-full rounded-lg object-cover bg-gray-300 group-hover:opacity-75 xl:aspect-7/8"
-              />
+              ></p>
               <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
               <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
-            </a>
+            </span>
           ))}
         </div>
       </div>
