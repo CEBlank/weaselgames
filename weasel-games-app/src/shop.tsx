3c56231dd1042ff8e07d50ecbc5757a935a586e5
Chris <96022511+CEBlank@users.noreply.github.com>
2025-11-09 21:25:02 -0600

    Update shop.tsx

--- weasel-games-app/src/shop.tsx
+++ weasel-games-app/src/shop.tsx
@@ -3,66 +3,66 @@ export function Shop() {
 const products = [
   {
     id: 1,
-    name: 'Earthen Bottle',
+    name: 'Dungeons&Dragons: Player Handbook 2024',
     href: '#',
-    price: '$48',
-    imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-01.jpg',
-    imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+    price: '$49.99',
+    imageSrc: '#',
+    imageAlt: 'Dungeons and Dragons player handbook 2024 edition.',
   },
   {
     id: 2,
-    name: 'Nomad Tumbler',
+    name: 'Root - A Game of Woodland Might and Right',
     href: '#',
-    price: '$35',
-    imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-02.jpg',
-    imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
+    price: '$60.00',
+    imageSrc: '#',
+    imageAlt: '#',
   },
   {
     id: 3,
-    name: 'Focus Paper Refill',
+    name: 'Arcadia Quest',
     href: '#',
-    price: '$89',
-    imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-03.jpg',
-    imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+    price: '$99.99',
+    imageSrc: '#',
+    imageAlt: '#',
   },
   {
     id: 4,
-    name: 'Machined Mechanical Pencil',
+    name: 'Pathfinder Bestiary',
     href: '#',
-    price: '$35',
-    imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-04.jpg',
-    imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+    price: '$49.99',
+    imageSrc: '#',
+    imageAlt: '#',
   },
   {
     id: 5,
-    name: 'Focus Card Tray',
+    name: 'Card Keeper',
     href: '#',
-    price: '$64',
-    imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-05.jpg',
-    imageAlt: 'Paper card sitting upright in walnut card holder on desk.',
+    price: '$18.00',
+    imageSrc: '#',
+    imageAlt: '#',
   },
   {
     id: 6,
     name: 'Focus Multi-Pack',
     href: '#',
-    price: '$39',
-    imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-06.jpg',
+    price: '$39.00',
+    imageSrc: '#',
     imageAlt: 'Stack of 3 small drab green cardboard paper card refill boxes with white text.',
   },
   {
     id: 7,
-    name: 'Brass Scissors',
+    name: 'Monster Manual 2024',
     href: '#',
-    price: '$50',
-    imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-07.jpg',
-    imageAlt: 'Brass scissors with geometric design, black steel finger holes, and included upright brass stand.',
+    price: '$50.00',
+    imageSrc: '#',
+    imageAlt: '#',
   },
   {
     id: 8,
     name: 'Focus Carry Pouch',
     href: '#',
-    price: '$32',
-    imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-08.jpg',
+    price: '$32.00',
+    imageSrc: '#',
     imageAlt: 'Textured gray felt pouch for paper cards with snap button flap and elastic pen holder loop.',
   },
 ]
@@ -71,7 +71,7 @@ const products = [
 
   return (
     <div className="">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 bg-white">
         <h2 className="sr-only">Products</h2>
 
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 bg-white">
@@ -80,7 +80,7 @@ const products = [
               <img
                 alt={product.imageAlt}
                 src={product.imageSrc}
-                className="aspect-square w-full rounded-lg object-cover group-hover:opacity-75 xl:aspect-7/8"
+                className="aspect-square w-full rounded-lg object-cover bg-gray-300 group-hover:opacity-75 xl:aspect-7/8"
               />
               <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
               <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
